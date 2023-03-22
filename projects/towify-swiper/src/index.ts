4a9755829808d02927583610c898bd2283ae9406
kaysaith <kaysaith@qq.com>
2023-03-22 16:39:35 +0800

    perf(Package): update dependencies

--- projects/towify-swiper/src/index.ts
+++ projects/towify-swiper/src/index.ts
@@ -5,3 +5,4 @@
 export * from './lib/towify-swiper.service';
 export * from './lib/towify-swiper.component';
 export * from './lib/towify-swiper.module';
+export * from './lib/towify-swiper-content.directive';
